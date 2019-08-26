d4d389e350090f419b65e2fe79bc53c3ef9139a7
Luis Carlos Silva Junior <rairaiwolf@outlook.com>
2019-08-26 12:05:43 -0300

    Implementing DAO
    Workin in progress over generic DAO.

--- src/main/java/br/com/mycompany/hibernatejpa/model/dao/DAO.java
+++ src/main/java/br/com/mycompany/hibernatejpa/model/dao/DAO.java
@@ -1,5 +1,10 @@
 package br.com.mycompany.hibernatejpa.model.dao;
 
+/**
+ * This is an interface of a generic DAO. A generic DAO will be util to persist every
+ * entity inside this application into database.
+ * @author Luis
+ */
 public interface DAO {
-
+	
 }
