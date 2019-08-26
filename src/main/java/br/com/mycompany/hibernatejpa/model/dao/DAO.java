7969cd61c1e806586dbe817cc646e866cc0064e6
Luis Carlos Silva Junior <rairaiwolf@outlook.com>
2019-08-26 20:53:03 -0300

    Update DAO.java
    Added some documentation on DAO.java to explain how does its methods works.

--- src/main/java/br/com/mycompany/hibernatejpa/model/dao/DAO.java
+++ src/main/java/br/com/mycompany/hibernatejpa/model/dao/DAO.java
@@ -5,39 +5,47 @@ import java.util.List;
 import br.com.mycompany.hibernatejpa.model.bean.AbstractBean;
 
 /**
- * 
+ * This is an interface that defines a DAO. A DAO object must implement all four basic
+ * operations of a database: find, findAll, remove and save. 
  * @author Luis
  *
- * @param <K>
- * @param <T>
+ * @param <K> Primary key type of a given Entity.
+ * @param <T> Type of Mapped instance object.
  */
 public interface DAO <K, T extends AbstractBean> {
 	/**
-	 * 
-	 * @param c
-	 * @param k
-	 * @return
+	 * Finds an entity on database given it's ID. If the entity exists, this method
+	 * will return an instance of entity mapped class with each attribute filled with
+	 * its corresponding table collumn.
+	 * @param c Mapped entity class.
+	 * @param k Entity primary key.
+	 * @return an instance of a mapped Entity class or null if it doesn't exists or if
+	 * an error occurs.
 	 */
 	public T find(Class<T> c, K k);
 	
 	/**
-	 * 
-	 * @param c
-	 * @return
+	 * Return a list of all entities persisted on the database until present moment.
+	 * @param c Entity class.
+	 * @return Return a list of instances of Mapped class c or null if an error
+	 * occurs.
 	 */
 	public List<T> findAll(Class<T> c);
 	
 	/**
-	 * 
-	 * @param t
-	 * @return
+	 * Remove an entity from database given its primary key. If the entity exists,
+	 * then it'll be deleted. Otherwise, nothing occurs.
+	 * @param c mapped entity class.
+	 * @param k entity primary key.
+	 * @return the removed entity, if exists. null otherwise or if an error occurs.
 	 */
 	public T remove(Class<T> c, K k);
 	
 	/**
-	 * 
-	 * @param t
-	 * @return
+	 * Persist an entity inside database or update it's information. Note that in
+	 * order to persist or update an existing entry.
+	 * @param t an entity to be persisted on database.
+	 * @return the same entity of argument with an aditional argument.
 	 */
 	public T save(T t);
 }
