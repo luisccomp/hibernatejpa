04cb083cb4f64e97e52c932c2c23a84e34222b1d
Luis Carlos Silva Junior <rairaiwolf@outlook.com>
2019-09-02 02:11:44 -0300

    Testando algumas relações
    Alguns relacionamentos estão funcionando. Testando mais outros depois.

--- src/main/java/br/com/mycompany/hibernatejpa/HibernateJPA.java
+++ src/main/java/br/com/mycompany/hibernatejpa/HibernateJPA.java
@@ -1,6 +1,7 @@
 package br.com.mycompany.hibernatejpa;
 
 import br.com.mycompany.hibernatejpa.model.bean.Category;
+import br.com.mycompany.hibernatejpa.model.bean.Product;
 import br.com.mycompany.hibernatejpa.model.dao.ConcreteDAO;
 import br.com.mycompany.hibernatejpa.model.dao.DAO;
 
@@ -11,10 +12,22 @@ import br.com.mycompany.hibernatejpa.model.dao.DAO;
 public class HibernateJPA {
 
 	public static void main(String[] args) {
-		DAO<Long, Category> dao = new ConcreteDAO<>();
-		Category category = dao.remove(Category.class, 3L);
+		// Instanciando os DAO's
+		DAO<Long, Product> productDAO = new ConcreteDAO<Long, Product>();
+		DAO<Long, Category> categoryDAO = new ConcreteDAO<Long, Category>();
 		
-		System.out.println("ID: " + category.getId());
-		System.out.println("Description: " + category.getDescription());
+		Product product = new Product();
+		product.setId(1L);
+		product.setName("Play Station 4");
+		product.setDescription("The powerful new generation console PS4.");
+		product.setQuantity(10);
+		product.setValue(1700.00);
+		
+		Category category = categoryDAO.find(Category.class, 1L);
+		
+		product.setCategory(category);
+		
+		// Persistindo o objeto no banco de dados
+		productDAO.save(product);
 	}
 }
